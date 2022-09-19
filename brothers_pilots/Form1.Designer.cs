9d9876a82d0b365d948fe329b2da9fb77dfcc808
VladislavChepusov <61112124+VladislavChepusov@users.noreply.github.com>
2022-09-19 22:19:00 +0300

    v1.0
    Осталось только пофиксить размер картинок

--- brothers_pilots/Form1.Designer.cs
+++ brothers_pilots/Form1.Designer.cs
@@ -29,31 +29,102 @@ namespace brothers_pilots
         /// </summary>
         private void InitializeComponent()
         {
-            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
-            // domainUpDown1
+            // panel1
             // 
-            this.domainUpDown1.Location = new System.Drawing.Point(12, 12);
-            this.domainUpDown1.Name = "domainUpDown1";
-            this.domainUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.domainUpDown1.TabIndex = 0;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(776, 44);
+            this.panel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(129, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Изменить размерность";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(329, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "teeeesssttt";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(12, 62);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 293);
+            this.panel2.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Холодильник Пилотов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DomainUpDown domainUpDown1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- brothers_pilots/Form1.cs
+++ brothers_pilots/Form1.cs
@@ -1,20 +1,239 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Media;
 using System.Windows.Forms;
+
 
 namespace brothers_pilots
 {
     public partial class Form1 : Form
     {
+        private int level = 2;//Сложность игры
+        private int size = 0;//размер отдельного рычага сейфа
+        private int xshift = 0;// Сдвиг кубок от углов панеле
+        private int yshift = 0;// Сдвиг кубок от углов панеле
+        SoundPlayer win_sound = new SoundPlayer(Properties.Resources.win);// Звук победы
+        SoundPlayer bye_sound = new SoundPlayer(Properties.Resources.bye);// Грустный тромбон
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        public Button[,] levers;//Рычаги
+        private void StartGame()
+        {
+            levers = new Button[level, level]; // Задаем массив кнопок 
+            int numberMix;
+            int x, y;
+            Random rand = new Random();
+            for (int i = 0; i < level; i++)
+            {
+                for (int j = 0; j < level; j++)
+                {
+                    levers[i, j] = new Button
+                    {
+                        Size = new Size(size, size),
+                        ImageKey ="0",
+                        Location = new Point(i*size + xshift, j*size+ yshift),
+                        Name = i.ToString() + ' ' + j.ToString(),
+                        BackColor = Color.White,
+                        Image = Properties.Resources.horizontal,
+                        BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom,
+                        
+                };
+                    levers[i, j].Click += new System.EventHandler(button_Click);
+                    panel2.Controls.Add(levers[i, j]);
+                }
+            }
+            //Пермеешивание 
+            do
+            {
+                numberMix = rand.Next(level, level + 20);
+                for (int num = 0; num < numberMix; num++)
+                {
+                    x = rand.Next(0, level);
+                    for (int j = 0; j < level; ++j)
+                        revers(levers[x, j]);
+                    y = rand.Next(0, level);
+                    for (int i = 0; i < level; ++i)
+                        revers(levers[i, y]);
+                    revers(levers[x, y]);
+                }
+            } while (CheckWin());   
+        }
+
+        
+
+        //Нажатие на рычаг
+        private void button_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            string[] strKoord = btn.Name.Split(' ');
+            int x = Convert.ToInt32(strKoord[0]);
+            int y = Convert.ToInt32(strKoord[1]);
+
+            for (int j = 0; j < level; j++)
+                revers(levers[x, j]);
+            for (int i = 0; i < level; i++)
+                revers(levers[i, y]);
+            revers(levers[x, y]);
+
+            if (CheckWin())
+                finish();
+        }
+
+        void finish()
+        {
+            win_sound.Play();
+            MessageBox.Show("Да вы медвежатник,шеф","МОЕ УВАЖЕНИЕ");
+            
+            for (int i = 0; i < level; ++i)
+                for (int j = 0; j < level; ++j)
+                {
+                    panel2.Controls.Remove(levers[i, j]);
+                }
+               
+            if (MessageBox.Show("Повышаем ставки?", "Вопросы к победилю", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                level += 1;
+                configuration();
+                StartGame();
+            }
+            else
+            {
+                bye_sound.Play();
+                MessageBox.Show("Всего хорошего!", "До новых встреч!");
+                Application.Exit();
+            }
+        }
+
+        bool CheckWin()
+        {
+            int up = 0;
+            int down = 0;
+            for (int i = 0; i < level; i++)
+                for (int j = 0; j < level; j++)
+                {
+                    if (levers[i, j].ImageKey == "0")
+                        down++;
+                    else up++;
+                }
+            if (down == level * level || up == level * level)
+                return true;
+            return false;
+        }
+
+
+        //Размер рычагов для вашего жкрана
+        private int sizecube(int mHeight, int mWidth)
+        {
+            return Math.Min(mHeight / level, mWidth / level);
+        }
+
+
+        //Переключение рычагов
+        void revers(Button button)
+        {
+            if (button.ImageKey == "0")
+            {
+                button.ImageKey = "1";
+                button.Image = Properties.Resources.vertical;
+            }
+            else
+            {
+                button.ImageKey = "0";
+                button.Image = Properties.Resources.horizontal;
+            }
+        }
+
+        // Функция расчета размеров экрана и размеров рычагов
+        private void configuration()
+        {
+            this.WindowState = FormWindowState.Maximized;
+            //this.TopMost = true;
+
+            panel1.Width = (int)(this.Width * 0.9);
+            panel2.Width = (int)(this.Width * 0.9);
+            panel1.Height = (int)(this.Height * 0.1);
+            panel2.Height = (int)(this.Height * 0.89);
+
+            label1.Text = $"Уровень {level - 1}";
+            label1.Left = panel1.Width / 2 - 50;
+
+
+            size = sizecube(panel2.Height, panel2.Width);
+
+            //не лучшая моя идея
+            xshift = (panel2.Width - level * size) / 2;
+            yshift = (panel2.Height - level * size) / 2;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            configuration();
+            StartGame();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < level; ++i)
+                for (int j = 0; j < level; ++j)
+                {
+                    panel2.Controls.Remove(levers[i, j]);
+                }
+            int nl;
+            if (int.TryParse(numericUpDown1.Value.ToString(), out nl))
+                level = nl;
+            else
+            {
+                MessageBox.Show("Неправльный ввод размерности...");
+                level = 2;
+            }
+            configuration();
+            StartGame();
+
+        }
+
+
+
+        /*
+          private int sizecube(int mHeight, int mWidth )
+          {
+              // int(sqrt(высота_прямоугольника*ширина_прямоугольника/количество_квадратов))
+              int MaxiSize = (int)(Math.Sqrt(mHeight * mWidth / level));
+
+              MessageBox.Show("Максимальная сторона квадрата =" + MaxiSize.ToString());
+              var numbers = new List<int>();
+              for (int i = 1; i <= level; i++)
+              {
+                  int W = (mWidth / i);
+                  int H = (mHeight / i);
+                  if (W <= MaxiSize && !numbers.Contains(W))
+                  {
+                      numbers.Add(W);
+                  }
+                   if (H <= MaxiSize && !numbers.Contains(H))
+                  {
+                      numbers.Add(H);
+                  }
+                  numbers.Sort();
+                  numbers.Reverse();
+              }
+              int q = 0;
+              MaxiSize = numbers[q];
+              int cube_count = (int)(mHeight / MaxiSize) + (int)(mWidth / MaxiSize);
+              while (cube_count < level)
+              {
+                  q += 1;
+                  MaxiSize = numbers[q];
+                  cube_count = (int)(mHeight / MaxiSize) + (int)(mWidth / MaxiSize);
+              }
+              return MaxiSize;
+          }
+       */
+
+
     }
 }
